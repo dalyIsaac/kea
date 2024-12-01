19ce3a4836712dc00cf29eeb6e9da6f4bf2baf71
Isaac Daly <isaac.daly@outlook.com>
2024-12-01 18:04:55 +1100

    Add Monaco editor

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createSignal, ErrorBoundary, Suspense } from "solid-js";
 import "./App.css";
 import { makePersisted } from "@solid-primitives/storage";
 import { createQuery } from "@tanstack/solid-query";
+import { MonacoDiffEditor } from "./editor/MonacoDiffEditor";
 
 const [api, setApi] = makePersisted(createSignal("https://api.github.com"), {
   storage: localStorage,
@@ -11,24 +12,99 @@ const [personalAccessToken, setPersonalAccessToken] = makePersisted(
   { storage: localStorage }
 );
 
+const [repository, setRepository] = makePersisted(
+  createSignal("microsoft/playwright")
+);
+const [pullRequest, setPullRequest] = makePersisted(createSignal(33645));
+
 function App() {
-  const repositoryQuery = createQuery(() => ({
+  const pullRequestDetailsQuery = createQuery(() => ({
     enabled: !!personalAccessToken(),
-    queryKey: ["repositories"],
+    queryKey: ["pullRequestDetails", repository(), pullRequest()],
     queryFn: async () => {
-      const response = await fetch(`${api()}/users/dalyisaac/repos`, {
-        headers: {
-          Authorization: `token ${personalAccessToken()}`,
-        },
-      });
+      const response = await fetch(
+        `${api()}/repos/${repository()}/pulls/${pullRequest()}`,
+        {
+          headers: {
+            Authorization: `token ${personalAccessToken()}`,
+          },
+        }
+      );
+      return response.json();
+    },
+    staleTime: 1000 * 60 * 5,
+    throwOnError: true,
+  }));
+
+  const pullRequestFilesQuery = createQuery(() => ({
+    enabled: !!personalAccessToken(),
+    queryKey: ["pullRequestFiles", repository(), pullRequest()],
+    queryFn: async () => {
+      const response = await fetch(
+        `${api()}/repos/${repository()}/pulls/${pullRequest()}/files`,
+        {
+          headers: {
+            Authorization: `token ${personalAccessToken()}`,
+          },
+        }
+      );
       return response.json();
     },
     staleTime: 1000 * 60 * 5, // 5 minutes
     throwOnError: true,
   }));
 
+  // Get the first file's content
+  const firstFileBlobQuery = createQuery(() => ({
+    enabled:
+      !!personalAccessToken() &&
+      !!pullRequestFilesQuery.data &&
+      !!pullRequestDetailsQuery.data,
+    queryKey: ["firstFileBlob", repository(), pullRequest()],
+    queryFn: async () => {
+      const files = pullRequestFilesQuery.data;
+      const firstFile = files[0];
+      const prDetails = pullRequestDetailsQuery.data;
+
+      // Get both modified and original content
+      const [modifiedResponse, originalResponse] = await Promise.all([
+        fetch(
+          `${api()}/repos/${repository()}/contents/${firstFile.filename}?ref=${
+            prDetails.head.sha
+          }`,
+          {
+            headers: {
+              Authorization: `token ${personalAccessToken()}`,
+              Accept: "application/vnd.github.v3.raw",
+            },
+          }
+        ),
+        fetch(
+          `${api()}/repos/${repository()}/contents/${firstFile.filename}?ref=${
+            prDetails.base.sha
+          }`,
+          {
+            headers: {
+              Authorization: `token ${personalAccessToken()}`,
+              Accept: "application/vnd.github.v3.raw",
+            },
+          }
+        ),
+      ]);
+
+      const [modified, original] = await Promise.all([
+        modifiedResponse.text(),
+        originalResponse.text(),
+      ]);
+
+      return { modified, original };
+    },
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    throwOnError: true,
+  }));
+
   return (
-    <div>
+    <div style={{ height: "80vh", width: "80vw" }}>
       <label for="api">API</label>
       <input
         id="api"
@@ -45,7 +121,23 @@ function App() {
         onInput={(e) => setPersonalAccessToken(e.currentTarget.value)}
       />
 
-      <ErrorBoundary
+      <label for="repository">Repository</label>
+      <input
+        id="repository"
+        type="text"
+        value={repository()}
+        onInput={(e) => setRepository(e.currentTarget.value)}
+      />
+
+      <label for="pullRequest">Pull Request</label>
+      <input
+        id="pullRequest"
+        type="number"
+        value={pullRequest()}
+        onInput={(e) => setPullRequest(e.currentTarget.valueAsNumber)}
+      />
+
+      {/* <ErrorBoundary
         fallback={(error) => (
           <div>
             <h1>Something went wrong</h1>
@@ -60,7 +152,30 @@ function App() {
             ))}
           </ul>
         </Suspense>
+      </ErrorBoundary> */}
+
+      {/* The files */}
+      <ErrorBoundary
+        fallback={(error) => (
+          <div>
+            <h1>Something went wrong</h1>
+            <pre>{error.message}</pre>
+          </div>
+        )}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <ul>
+            {pullRequestFilesQuery.data?.map((file: any) => (
+              <li>{file.filename}</li>
+            ))}
+          </ul>
+        </Suspense>
       </ErrorBoundary>
+
+      <MonacoDiffEditor
+        original={firstFileBlobQuery.data?.original ?? ""}
+        modified={firstFileBlobQuery.data?.modified ?? ""}
+      />
     </div>
   );
 }
