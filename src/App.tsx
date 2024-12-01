7a6bcc05ad8d76573463c90d5a3407669b5d2b27
Isaac Daly <isaac.daly@outlook.com>
2024-12-01 13:54:16 +1100

    Add Tanstack Query to interact with the GitHub API

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,67 @@
-import { createSignal } from "solid-js";
-import solidLogo from "./assets/solid.svg";
-import viteLogo from "/vite.svg";
+import { createSignal, ErrorBoundary, Suspense } from "solid-js";
 import "./App.css";
 import { makePersisted } from "@solid-primitives/storage";
+import { createQuery } from "@tanstack/solid-query";
 
 const [api, setApi] = makePersisted(createSignal("https://api.github.com"), {
   storage: localStorage,
 });
+const [personalAccessToken, setPersonalAccessToken] = makePersisted(
+  createSignal(""),
+  { storage: localStorage }
+);
 
 function App() {
+  const repositoryQuery = createQuery(() => ({
+    enabled: !!personalAccessToken(),
+    queryKey: ["repositories"],
+    queryFn: async () => {
+      const response = await fetch(`${api()}/users/dalyisaac/repos`, {
+        headers: {
+          Authorization: `token ${personalAccessToken()}`,
+        },
+      });
+      return response.json();
+    },
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    throwOnError: true,
+  }));
+
   return (
-    <>
-      <header class="App-header">
-        <img src={solidLogo} class="App-logo" alt="logo" />
-        <img src={viteLogo} class="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <input
-          type="text"
-          value={api()}
-          onInput={(e) => setApi(e.currentTarget.value)}
-        />
-        <a
-          class="App-link"
-          href={api()}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </>
+    <div>
+      <label for="api">API</label>
+      <input
+        id="api"
+        type="text"
+        value={api()}
+        onInput={(e) => setApi(e.currentTarget.value)}
+      />
+
+      <label for="personalAccessToken">Personal Access Token</label>
+      <input
+        id="personalAccessToken"
+        type="text"
+        value={personalAccessToken()}
+        onInput={(e) => setPersonalAccessToken(e.currentTarget.value)}
+      />
+
+      <ErrorBoundary
+        fallback={(error) => (
+          <div>
+            <h1>Something went wrong</h1>
+            <pre>{error.message}</pre>
+          </div>
+        )}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <ul>
+            {repositoryQuery.data?.map((repo: any) => (
+              <li>{repo.full_name}</li>
+            ))}
+          </ul>
+        </Suspense>
+      </ErrorBoundary>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@
 import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 
 const root = document.getElementById("root");
+const client = new QueryClient();
 
-render(() => <App />, root!);
+render(
+  () => (
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
+  ),
+  root!
+);
