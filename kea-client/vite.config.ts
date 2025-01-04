3a43aa3de9f5b41103784177ed2c49cd77f20265
Isaac Daly <isaac.daly@outlook.com>
2025-01-04 22:03:47 +0000

    Initial React commit

--- kea-client/vite.config.ts
+++ kea-client/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [solid(), tsconfigPaths()],
-});
+  plugins: [TanStackRouterVite({}), react()],
+})
