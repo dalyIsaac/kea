b9c7df778b437d1b07770c656ba1f5ac41e7d83d
Isaac Daly <isaac.daly@outlook.com>
2025-03-16 16:58:10 +1100

    Move the coverage config

--- kea-vscode/.vscode-test.mjs
+++ kea-vscode/.vscode-test.mjs
@@ -2,4 +2,9 @@ import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
   files: "out/**/*.test.js",
+  srcDir: "src",
+  coverage: {
+    includeAll: true,
+    reporter: ["lcov"],
+  },
 });
