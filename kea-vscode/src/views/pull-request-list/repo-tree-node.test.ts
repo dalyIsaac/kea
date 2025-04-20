bcb50de45091656eb9a5c97e8886bfdec781c0de
Isaac Daly <isaac.daly@outlook.com>
2025-04-20 18:21:39 +1000

    RepoTreeNode tests

---
+++ kea-vscode/src/views/pull-request-list/repo-tree-node.test.ts
@@ -0,0 +1,101 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+import { IAccount, IAccountKey } from "../../account/account";
+import { createPullRequestStub, createRepositoryStub, createWorkspaceFolderStub } from "../../test-utils";
+import { PullRequestListNode } from "./pull-request-list-node";
+import { RepoTreeNode } from "./repo-tree-node";
+
+suite("RepoTreeNode", () => {
+  const accountKey: IAccountKey = { providerId: "github", accountId: "test-account" };
+
+  test("should be created with the correct properties", () => {
+    // Given
+    const mockRepository = createRepositoryStub({
+      remoteUrl: "https://github.com/owner/repo",
+      account: { accountKey } as IAccount,
+    });
+    const mockWorkspaceFolder = createWorkspaceFolderStub({
+      uri: vscode.Uri.file("c:/test/workspace"),
+      name: "test-workspace",
+    });
+
+    // When
+    const repoTreeNode = new RepoTreeNode(mockRepository, mockWorkspaceFolder);
+
+    // Then
+    assert.strictEqual(repoTreeNode.repository, mockRepository);
+    assert.strictEqual(repoTreeNode.workspace, mockWorkspaceFolder);
+    assert.strictEqual(repoTreeNode.collapsibleState, "collapsed");
+  });
+
+  test("getTreeItem returns a TreeItem with the correct properties", () => {
+    // Given
+    const mockRepository = createRepositoryStub({
+      remoteUrl: "https://github.com/owner/repo",
+      account: { accountKey } as IAccount,
+    });
+    const mockWorkspaceFolder = createWorkspaceFolderStub({
+      uri: vscode.Uri.file("c:/test/workspace"),
+      name: "test-workspace",
+    });
+    const repoTreeNode = new RepoTreeNode(mockRepository, mockWorkspaceFolder);
+
+    // When
+    const treeItem = repoTreeNode.getTreeItem();
+
+    // Then
+    assert.strictEqual(treeItem.label, "test-workspace");
+    assert.strictEqual(treeItem.collapsibleState, vscode.TreeItemCollapsibleState.Collapsed);
+    assert.strictEqual(treeItem.contextValue, "repository");
+    assert.strictEqual(treeItem.description, "https://github.com/owner/repo");
+  });
+
+  test("getChildren returns PullRequestListNodes on success", async () => {
+    // Given
+    const pullRequest1 = createPullRequestStub({ number: 1, title: "PR 1" });
+    const pullRequest2 = createPullRequestStub({ number: 2, title: "PR 2" });
+    const mockRepository = createRepositoryStub({
+      remoteUrl: "https://github.com/owner/repo",
+      account: { accountKey } as IAccount,
+      getPullRequestList: () => Promise.resolve([pullRequest1, pullRequest2]),
+    });
+    const mockWorkspaceFolder = createWorkspaceFolderStub({
+      uri: vscode.Uri.file("c:/test/workspace"),
+      name: "test-workspace",
+    });
+    const repoTreeNode = new RepoTreeNode(mockRepository, mockWorkspaceFolder);
+
+    // When
+    const children = await repoTreeNode.getChildren();
+
+    // Then
+    assert.strictEqual(children.length, 2);
+    assert.ok(children[0] instanceof PullRequestListNode);
+    assert.strictEqual(children[0].pullRequest, pullRequest1);
+    assert.strictEqual(children[0].accountKey, accountKey);
+    assert.ok(children[1] instanceof PullRequestListNode);
+    assert.strictEqual(children[1].pullRequest, pullRequest2);
+    assert.strictEqual(children[1].accountKey, accountKey);
+  });
+
+  test("getChildren returns empty array on failure", async () => {
+    // Given
+    const error = new Error("Failed to fetch PRs");
+    const mockRepository = createRepositoryStub({
+      remoteUrl: "https://github.com/owner/repo",
+      account: { accountKey } as IAccount,
+      getPullRequestList: () => Promise.resolve(error),
+    });
+    const mockWorkspaceFolder = createWorkspaceFolderStub({
+      uri: vscode.Uri.file("c:/test/workspace"),
+      name: "test-workspace",
+    });
+    const repoTreeNode = new RepoTreeNode(mockRepository, mockWorkspaceFolder);
+
+    // When
+    const children = await repoTreeNode.getChildren();
+
+    // Then
+    assert.strictEqual(children.length, 0);
+  });
+});
